c8b8f37b5a333a1bad6246fae7990f757bbb78bf
Heru Gunawan <heru07g@gmail.com>
2021-12-28 14:13:07 +0700

    refactor: move folders inside src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {

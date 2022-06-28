44e704256f0cea4f579f3e57707bed4f661d425d
bnhr07 <heru07g@gmail.com>
2022-06-28 15:02:59 +0700

    chore: try migrate to pnpm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ function Home() {
   return (
     <Layout>
       <p className="flex flex-col items-center justify-between text-3xl font-semibold text-blue-900">
-        Hello from Next.js and <span className="font-bold">Tailwind CSS</span>
+        Hello from <span className="text-medium">Next.js</span> and{' '}
+        <span className="font-bold">Tailwind CSS</span>
       </p>
     </Layout>
   )
